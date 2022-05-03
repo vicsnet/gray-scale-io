b3133d18f1ea288c31be0184e791d5611e327a3d
Mahdi <muhdmahdia36@gmail.com>
2022-05-03 02:20:54 +0100

    Videos player added

--- src/components/layouts/LearnMore.tsx
+++ src/components/layouts/LearnMore.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import { useEffect } from 'react';
+import ReactPlayer from 'react-player';
+
+const url = 'https://www.youtube.com/watch?v=YQmqdpkFwYI';
 
 const LearnMore = () => {
+  useEffect(() => {
+    const onReady = () => {};
+  }, []);
+
   return (
     <div className='text-center'>
       <h1 className='text-bold'>Learn more about GreyScale.io</h1>
-      <video
-        src='../../../../../Movies/Swagger/Season 1/Swagger S01E01 - NBA.mp4'
-        height={200}
-        width={300}
-        className='w-full rounded-2xl'
-        controls={true}
-        autoPlay
-      />
+      <ReactPlayer width={'100%'} height='480px' url={url} controls />
     </div>
   );
 };
