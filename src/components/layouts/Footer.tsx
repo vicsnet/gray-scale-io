785abcdb1c3e8ff1dd0426a4a8f82ac1d4626866
Mahdi <muhdmahdia36@gmail.com>
2022-05-03 02:27:02 +0100

    Video palyer added

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,24 +1,27 @@
 const Footer = () => {
   return (
-    <div className='footer '>
-      <div className='bg'>
-        <div className='content'>
-          <h1>
-            Subscribe to get updates about new releases, air drops, deals and
-            tips anf tricks
-          </h1>
-          <p>Your data is safe with us</p>
-          <form className='form'>
-            <input
-              type='text'
-              className='text-input'
-              placeholder='enter your email'
-            />
-            <input type='submit' value='Subscribe' className='btn' />
-          </form>
+    <>
+      <div className='footer'>
+        <div className='bg'>
+          <div className='content'>
+            <h1>
+              Subscribe to get updates about new releases, air drops, deals and
+              tips anf tricks
+            </h1>
+            <p>Your data is safe with us</p>
+            <form className='form'>
+              <input
+                type='text'
+                className='text-input'
+                placeholder='enter your email'
+              />
+              <input type='submit' value='Subscribe' className='btn' />
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+      <p className='text-center py-5'>Team Grayscale</p>
+    </>
   );
 };
 
--- src/components/layouts/LearnMore.tsx
+++ src/components/layouts/LearnMore.tsx
@@ -1,17 +1,18 @@
-import { useEffect } from 'react';
 import ReactPlayer from 'react-player';
 
 const url = 'https://www.youtube.com/watch?v=YQmqdpkFwYI';
 
 const LearnMore = () => {
-  useEffect(() => {
-    const onReady = () => {};
-  }, []);
-
   return (
     <div className='text-center'>
       <h1 className='text-bold'>Learn more about GreyScale.io</h1>
-      <ReactPlayer width={'100%'} height='480px' url={url} controls />
+      <ReactPlayer
+        width={'100%'}
+        height='480px'
+        url={url}
+        controls
+        style={{ borderRadius: '20px' }}
+      />
     </div>
   );
 };
